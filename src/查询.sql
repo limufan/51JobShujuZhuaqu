1c72267c28b71270504be90498fa4cc905b88142
limufan <214528367@qq.com>
2017-02-23 15:30:07 +0800

    修改查询sql

--- src/查询.sql
+++ src/查询.sql
@@ -1,18 +1,40 @@
 select count(1) from dbo.job_zhiwei
-where zhiwei_gongzuoDidian like '%佛山%'
+where zhiwei_gongzuoDidian like '%成都%'
 
 select top 100 zhiwei_name, zhiwei_gongsiName, zhiwei_zuigaoGongzi, zhiwei_gongzuoDidian, zhiwei_lianjie 
 from dbo.job_zhiwei
 where 
-zhiwei_gongzuoDidian like '%荔湾%' and 
+zhiwei_gongzuoDidian like '%成都%' and 
 zhiwei_zuigaoGongzi >= 12000
-and zhiwei_zuigaoGongzi <= 20000
---and zhiwei_mingxi like '%吃%住%'
+--and zhiwei_zuigaoGongzi <= 20000
+and zhiwei_mingxi like '%吃%'
+order by zhiwei_zuigaoGongzi desc
+
+
+select top 100 zhiwei_name, zhiwei_gongsiName, zhiwei_zuigaoGongzi, zhiwei_gongzuoDidian, zhiwei_lianjie 
+from dbo.job_zhiwei
+where 
+zhiwei_gongzuoDidian like '%重庆%' and 
+zhiwei_zuigaoGongzi >= 12000
+--and zhiwei_zuigaoGongzi <= 20000
+--and zhiwei_mingxi like '%吃%'
 order by zhiwei_zuigaoGongzi desc
 
 select top 100 zhiwei_name, zhiwei_gongsiName, zhiwei_zuigaoGongzi, zhiwei_gongzuoDidian, zhiwei_lianjie 
 from dbo.job_zhiwei
 where day(zhiwei_createdTime) = day(GETDATE())
+and zhiwei_gongzuoDidian like '%成都%'
 order by zhiwei_zuigaoGongzi desc
 
 
+select *from job_zhiwei
+
+
+select top 100 '<div><a target="_blank" href='+zhiwei_lianjie+'>' + zhiwei_name + '</a>', 'gs===' + zhiwei_gongsiName +'gz===', zhiwei_zuigaoGongzi, zhiwei_gongzuoDidian 
+from dbo.job_zhiwei
+where 
+zhiwei_gongzuoDidian like '%重庆%' and 
+zhiwei_zuigaoGongzi >= 12000
+--and zhiwei_zuigaoGongzi <= 20000
+--and zhiwei_mingxi like '%吃%'
+order by zhiwei_zuigaoGongzi desc
